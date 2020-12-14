0b3a45b2d07000c87148c865729c452295891f50
radish-miyazaki <y.hidaka.kobe@gmail.com>
2020-12-14 18:14:11 +0900

    implement register

--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -1,24 +1,50 @@
 <template>
-  <form class="form-signin">
+  <form class="form-signin" @submit.prevent="submit">
     <h1 class="h3 mb-3 font-weight-normal">Please register</h1>
 
-    <input class="form-control" placeholder="First Name" required/>
+    <input v-model="first_name" class="form-control" placeholder="First Name" required/>
 
-    <input class="form-control" placeholder="Last Name" required/>
+    <input v-model="last_name" class="form-control" placeholder="Last Name" required/>
 
-    <input type="email" class="form-control" placeholder="Email" required/>
+    <input v-model="email" type="email" class="form-control" placeholder="Email" required/>
 
-    <input type="password" class="form-control" placeholder="Password" required/>
+    <input v-model="password" type="password" class="form-control" placeholder="Password" required/>
 
-    <input type="password" class="form-control" placeholder="Password Confirm" required/>
+    <input  v-model="password_confirm" type="password" class="form-control" placeholder="Password Confirm" required/>
 
     <button class="btn btn-lg btn-primary btn-block" type="submit">Register</button>
   </form>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-  name: "Register"
+  name: "Register",
+
+  data() {
+    return {
+      first_name: '',
+      last_name: '',
+      email: '',
+      password: '',
+      password_confirm: '',
+    };
+  },
+
+  methods: {
+    async submit() {
+      await axios.post('register', {
+        first_name: this.first_name,
+        last_name: this.last_name,
+        email: this.email,
+        password: this.password,
+        password_confirm: this.password_confirm,
+      });
+
+      await this.$router.push('/login');
+    }
+  }
 }
 </script>
 
