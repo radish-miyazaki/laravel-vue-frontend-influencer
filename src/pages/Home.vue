2adea77eafb743700e160131f0d3fd13adc87d8c
radish-miyazaki <y.hidaka.kobe@gmail.com>
2020-12-14 19:54:00 +0900

    create Product class

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -4,17 +4,13 @@
     <div class="album py-5 bg-light">
       <div class="container">
         <div class="row">
-          <div class="col-md-4">
+          <div class="col-md-4" v-for="(product, i) in products" :key="i">
             <div class="card mb-4 shadow-sm">
-              <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
+              <img :src="product.img" height="200" />
               <div class="card-body">
-                <p class="card-text">これは写真の解説文付きのカードです。自然に説明を加えることができます。しかしこの文章は少し長いかもしれません。</p>
+                <p class="card-text">{{ product.title }}</p>
                 <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group">
-                    <button type="button" class="btn btn-sm btn-outline-secondary">見る</button>
-                    <button type="button" class="btn btn-sm btn-outline-secondary">編集</button>
-                  </div>
-                  <small class="text-muted">9分</small>
+                  <small class="text-muted">${{ product.price }}}</small>
                 </div>
               </div>
             </div>
@@ -27,12 +23,25 @@
 
 <script>
 import Header from "@/components/Header.vue";
+import axios from "axios";
 
 export default {
   name: "Home",
 
   components: {
     Header,
+  },
+
+  data() {
+    return {
+      products: []
+    }
+  },
+
+  async mounted() {
+    const { data } = await axios.get('products');
+
+    this.product = data.data;
   }
 
 }
